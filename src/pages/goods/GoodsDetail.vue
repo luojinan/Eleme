5d02407c96eb7ea754c3a95052f6d1e542b7fba9
Your Name <1017386624@qq.com>
2018-09-15 15:38:19 +0800

    完成商品详情页面，所有逻辑，选项卡、添加购物车等，遇到bug无法定位bottom，还差把评论区的时间格式化

--- src/pages/goods/GoodsDetail.vue
+++ src/pages/goods/GoodsDetail.vue
@@ -44,19 +44,30 @@
 					:ratings="selectFood.ratings"
 					@changeType="changeType"
 					@changeSwitch="changeSwitch"
-					></rating-selected>
+				>						
+				</rating-selected>
 				<div class="goods-detail_rating--content">
-					<ul v-show="selectType.ratings && selectType.ratings.length">
-						<li v-for="item of selectType.ratings" class="">
+					<ul v-show="selectFood.ratings && selectFood.ratings.length>0">
+						<li 
+							v-for="item in selectFood.ratings" 
+							class="goods-detail_rating--item"
+							v-show="needShow(item.rateType,item.text)"
+						>
 							<div class="goods-detail_rating--user">
-								<span>{{item.username}}</span>
-								<img src="item.avatat" width="12" height="12">
+								<p>{{item.username}}</p>
+								<img :src="item.avatar" width="12" height="12">
 							</div>
 							<div class="goods-detail_rating--time">{{item.rateTime}}</div>
-							<p><span>{{item.text}}</span></p>
+							<p>
+								<span 
+									class="goods-detail_rating--iflike"
+									:class="{'goods-detail_rating--like':item.rateType===0}"
+								>♥</span>
+								{{item.text}}
+							</p>
 						</li>
 					</ul>
-					<div v-show="!selectType.ratings || selectType.ratings.length===0">
+					<div v-show="!selectFood.ratings || selectFood.ratings.length===0">
 						没有评价
 					</div>
 				</div>
@@ -112,25 +123,45 @@ export default {
 		handleAddFirst(){
 			Vue.set(this.selectFood,'count',1)
 		},
-		_myScroll(){
-			this.$nextTick(()=> {
-				if(!this.scroll){
-					this.scroll = new BScroll(this.$refs.detailScroll)
-				}
-			})
-		},
-		//自组件点击，修改状态码
+		//子组件点击，修改状态码
 		changeSwitch(switchType){
 			this.onlyContent = !switchType;
+			//当dom刷新的时候better-scroll需要刷新
+			this.$nextTick(()=> {
+				this.scroll.refresh()
+			})
 		},
 		changeType(type){
 			this.selectType = type ;
+			//当dom刷新的时候better-scroll需要刷新
+			this.$nextTick(()=> {
+				this.scroll.refresh()
+			})
 		},
-		
+		//根据状态码显示相应内容
+		needShow(type,text){
+			//传入评论数据的状态码（与点击产生的匹配），是否有内容的评论
+			if(!text && this.onlyContent){
+				//当前评论没有内容 点击了只显示有内容按钮时
+				return false
+			}
+			if(this.selectType === ALL){
+				//当前现实所有
+				return true
+			}else{
+				//否则以选择和当前评论状态匹配 true fasle
+				return this.selectType === type;
+			}
+		}
 	},
-	watch:{
-		selectFood:'_myScroll'
-	},
+	created(){
+		this.$nextTick(()=> {
+				if(!this.scroll){
+					this.scroll = new BScroll(this.$refs.detailScroll)
+				}
+				console.log(this.selectFood.ratings)
+			})
+	}
 
 
 }
@@ -139,13 +170,11 @@ export default {
 <style>
 /*页面外部布局*/
 	.goods-detail{
-		position: fixed;
+		position: absolute;
+		/*不知道为什么定位了top，之后定位不了bottom*/
+		bottom:48px;
 		top: 0;
-		left: 0;
-		bottom: 0;
-		right: 0;
 		overflow: hidden;
-		bottom: 48px;
 		width: 100%;
 		height: 100%;
 		background-color: #fff;
@@ -260,5 +289,52 @@ export default {
 		margin-left: 18px;
 		font-size: 14px;
 		color: rgb(7,17,27);
+	}
+/*商品内容样式*/
+	.goods-detail_rating--content{
+		padding: 0 18px;
+	}
+	.goods-detail_rating--item{
+		position: relative;
+		padding: 16px 0;
+		border-bottom: 1px solid rgba(7,17,27,0.1);
+	}
+	.goods-detail_rating--user{
+		position: absolute;
+		right: 0;
+		top: 16px;
+		line-height: 12px;
+		font-size: 0;
+	}
+	.goods-detail_rating--user p{
+		display: inline-block;
+		vertical-align: top;
+		font-size: 10px;
+		margin-right: 6px;
+		color: rgb(147,153,159);
+	}
+	.goods-detail_rating--user img{
+		border-radius: 50%;
+	}
+	.goods-detail_rating--time{
+		margin-bottom: 6px;
+		font-size: 10px;
+		line-height: 12px;
+		color: rgb(147,153,159);
+	}
+	.goods-detail_rating--item p{
+		line-height: 16px;
+		font-size: 12px;
+		color: rgb(7,17,27);
+	}
+	/*动态样式好评红心，差评灰色*/
+	.goods-detail_rating--iflike{
+		font-size:20px;
+		line-height: 24px;
+		margin-right: 2px; 
+		color: rgb(147,153,159);
+	}
+	.goods-detail_rating--like{
+		color: red;
 	}
 </style>
